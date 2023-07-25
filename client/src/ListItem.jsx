037b0e9bfa932ecb4ccc22f3e1ddd952b52a9c3d
Parth Dalwadi <dalwadip77@gmail.com>
2023-07-25 19:06:21 -0400

    Updated frontend styling and changed the order of elements in ListItem.

--- client/src/ListItem.jsx
+++ client/src/ListItem.jsx
@@ -1,15 +1,15 @@
 const ListItem = ({item, handleCheck, handleDelete}) => {
   return (
     <li className="task">
+        <label
+            style={(item.is_checked) ? {textDecoration: 'line-through'} : null}
+        >{item.description}</label>
+
         <input 
             type="checkbox"
             onChange={() => handleCheck(item.task_id)}
             checked={item.is_checked}
         />
-        
-        <label
-            style={(item.is_checked) ? {textDecoration: 'line-through'} : null}
-        >{item.description}</label>
 
         <button
             onClick={() => handleDelete(item.task_id)}
