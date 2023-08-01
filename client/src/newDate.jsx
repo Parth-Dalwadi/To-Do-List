51e10ae2481079dd4a8123d5b81bda2fdcaacee0
Parth Dalwadi <dalwadip77@gmail.com>
2023-08-01 00:41:10 -0400

    Updated newDate to add a 0 to the front of a single digit number.

--- client/src/newDate.jsx
+++ client/src/newDate.jsx
@@ -1,6 +1,15 @@
 const newDate = () => {
     let date = new Date().toLocaleDateString()
     date = date.split("/")
+
+    if (date[0].length === 1) {
+        date[0] = "0" + date[0]
+    }
+
+    if (date[1].length === 1) {
+        date[1] = "0" + date[1]
+    }
+
     date = date[2] + "-" + date[0] + "-" + date[1]
     return date
 }
